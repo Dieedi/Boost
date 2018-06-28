ef05d1f05e4adba77842b24fb1fb5ea6e83464a4
Jeremy 'Dieedi' Pouillot <pouillot.j@free.fr>
2018-06-29 01:15:07 +0200

    save

--- Boost/Assets/Rocket.cs
+++ Boost/Assets/Rocket.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Rocket : MonoBehaviour {
 
+	[SerializeField] private float rcsThrust = 75;
+	[SerializeField] private float rcsVThrust_Multiplier = 10; 
+	[SerializeField] private float Thruster = 750;
+	[SerializeField] private AudioClip ThrustSound;
+	[SerializeField] private AudioClip GazSound;
 	private float MoveH = 0;
 	private float MoveV = 0;
+	private bool MoveMod = false;
+	private bool SoundToggle = false;
 	private Rigidbody rigidbody;
 	private AudioSource audioSource;
 
@@ -27,33 +34,78 @@ public class Rocket : MonoBehaviour {
 	private void Thrust()
 	{
 		if (Input.GetButton("Jump")) {
-			rigidbody.AddRelativeForce(Vector3.up);
+			float ThrusterSpeed = Thruster * Time.deltaTime;
+			rigidbody.AddRelativeForce(Vector3.up * ThrusterSpeed);
 
-			if (!audioSource.isPlaying) {
-				audioSource.Play();
-			} else {
-				audioSource.mute = false;
-			}
+			if(!audioSource.isPlaying)
+				PlaySound(ThrustSound);
 		} else {
-			if (audioSource.isPlaying) {
-				audioSource.mute = true;
-			}
+			if (audioSource.isPlaying)
+				StopSound();
 		}
 	}
 
 	private void Rotate()
 	{
-		rigidbody.freezeRotation = true;
+		//rigidbody.freezeRotation = true;
 
 		MoveH = Input.GetAxis("Horizontal");
 		MoveV = Input.GetAxis("Vertical");
+		MoveMod = Input.GetButton("Straf_modifier");
 
-		if (MoveH > 0) {
-			transform.Rotate(-Vector3.forward);
-		} else if (MoveH < 0) {
-			transform.Rotate(Vector3.forward);
+		float rotationSpeed = rcsThrust * Time.deltaTime;
+		
+		if (!MoveMod && MoveH != 0) {
+			isMoving = true;
+			PlaySound(GazSound);
+
+			if (MoveH > 0) {
+				transform.Rotate(-Vector3.forward * rotationSpeed);
+			} else if (MoveH < 0) {
+				transform.Rotate(Vector3.forward * rotationSpeed);
+			}
+		} else if (MoveMod && (MoveH != 0 || MoveV != 0)) {
+
+			PlaySound(GazSound);
+
+			if (MoveH > 0) {
+				rigidbody.AddRelativeForce(-Vector3.right * rotationSpeed);
+			} else if (MoveH < 0) {
+				rigidbody.AddRelativeForce(Vector3.right * rotationSpeed);
+			}
+
+			if (MoveV > 0) {
+				rigidbody.AddRelativeForce(Vector3.up * rotationSpeed * rcsVThrust_Multiplier);
+			} else if (MoveV < 0) {
+				rigidbody.AddRelativeForce(-Vector3.up * rotationSpeed * rcsVThrust_Multiplier);
+			}
 		}
 
-		rigidbody.freezeRotation = false;
+		//rigidbody.freezeRotation = false;
+	}
+
+	private void StopSound()
+	{
+		audioSource.Stop();
+		audioSource.clip = null;
+	}
+
+	private void PlaySound (AudioClip SoundToPlay)
+	{
+		Debug.Log("should play " + SoundToPlay.name);
+		audioSource.clip = SoundToPlay;
+		audioSource.Play();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		switch (collision.gameObject.tag) {
+			case "Friendly":
+				Debug.Log("Landed");
+				break;
+			default:
+				Debug.Log("Die");
+				break;
+		}
 	}
 }
